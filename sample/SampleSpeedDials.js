7b80dcd9309ff3d0f6b590b1afd851a089c7da16
Arnaud Coquelet <arcoquel@gmail.com>
2013-09-15 12:42:08 -0400

    added sample js file

---
+++ sample/SampleSpeedDials.js
@@ -0,0 +1,44 @@
+[
+ { 'name' : 'Team',
+  'list' : [
+  			{'name' : 'OnCall', 'list' : [{'name':'Manager', 'number' : '+14121112222'},
+                                      {'name':'Voice', 'number' : '+14121112222'},
+                                      {'name':'Voice Escalation', 'number' : '+14121112222'},
+                                      {'name':'Data', 'number' : '+14121112222'},
+                                      {'name':'Data Escalation', 'number' : '+14121112222'} ]
+                                    },
+        {'name' : 'Voice', 'list' : [{'name':'Usr 1', 'number' : '+14121112222'}
+                                    ]},
+        {'name' : 'Data', 'list' : [{'name':'Data 1', 'number' : '+14121112222'},
+                                    {'name':'Data 2', 'number' : '+14121112222'},
+                                    {'name':'Data 3', 'number' : '+14121112222'}
+                                    ]}
+		   ]
+ },
+ { 'name' : 'Technical Support',
+  'list' : [
+            {'name' : 'GNOC', 'list' : [  {'name':'GNOC Bridge', 'number' : '+14121112222'},
+                                          {'name':'GNOC Poland', 'number' : '+14121112222'},
+                                          {'name':'GNOC Plano', 'number' : '+14121112222'}
+                                        ]
+                                        },
+            {'name' : 'A2K', 'list' : [   {'name':'A2K', 'number' : '+14121112222'},
+                                          {'name':'A2K Commercial', 'number' : '+14121112222'}
+                                      ]
+             },
+             {'name' : 'AVST', 'list' : [   {'name':'AVST', 'number' : '+14121112222'}
+                                      ]
+             }
+            ]
+ },
+{ 'name' : 'Lab test',
+  'list' : [
+                        {'name' : 'Bridge', 'list' : [
+                                        {'name':'MyTeamwork Participant', 'number' : '6053998$P155555#'},
+                                        {'name':'MyTeamwork Leader', 'number' : '6053998$P144444#'},
+                                      ]}
+                   ]
+ }
+
+]
+
